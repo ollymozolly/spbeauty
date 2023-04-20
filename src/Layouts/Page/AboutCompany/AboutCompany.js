74da291973165294f4393e2fdb69973de5168cc6
ollymozolly <130170469+ollymozolly@users.noreply.github.com>
2023-04-20 06:24:16 +0000

    add 2 cols about company

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -4,12 +4,13 @@ import Addresses from "./Components/Addresses/Addresses";
 function AboutCompany() {
     return (
         <div>
+            <div className='row d-flex justify-content-between gap-5 mx-5'>
             <Heading/> {/* Заголовок и кнопка справа */}
-            <div className='d-flex flex-column mt-5'>
-                <General/>  {/* Основная информация вроде названия, типа и телефона */}
-                <Addresses/> {/* Список доступных адресов заведений */}
+             <div className='col'><General/>  {/* Основная информация вроде названия, типа и телефона */}</div>
+            <div className='col'><Addresses/> {/* Список доступных адресов заведений */}</div>
             </div>
         </div>
+        
     );
 }
 
